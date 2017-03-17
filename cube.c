862445523c20abb04c5e2381122ec924501cb333
ananda kumar hn <andu1989anand@gmail.com>
2017-03-17 12:32:44 +0530

    Create cube.c

---
+++ cube.c
@@ -0,0 +1,126 @@
+/*8. Program to draw a color cube and allow the user to move the camera suitably to experiment with perspective viewing. Use OpenGL functions.*/
+
+#include<GL/glut.h>
+GLfloat vertices[][3]={{-1,-1,-1},
+{1,-1,-1},
+{1,1,-1},
+{-1,1,-1},
+{-1,-1,1},
+{1,-1,1},
+{1,1,1},
+{-1,1,1}};
+
+GLfloat colors[][3]={{0,0,1},
+{0,1,0},
+{0,1,1},
+{1,0,0},
+{1,0,1},
+{1,1,0},
+{1,1,1},
+{1,0.5,0}};
+
+static GLfloat theta[]={0,0,0};
+static GLint axis=2;
+static GLdouble viewer[]={0,0,5};
+
+void polygon(int a,int b,int c,int d)
+{
+	glBegin(GL_POLYGON);
+
+	glColor3fv(colors[a]);
+	glVertex3fv(vertices[a]);
+	glColor3fv(colors[b]);
+	glVertex3fv(vertices[b]);
+	glColor3fv(colors[c]);
+	glVertex3fv(vertices[c]);
+	glColor3fv(colors[d]);
+	glVertex3fv(vertices[d]);
+
+	glEnd();
+}
+
+void colorcube()
+{
+	polygon(0,1,2,3);
+	polygon(4,5,6,7);
+	polygon(0,4,7,3);
+	polygon(1,5,6,2);
+	polygon(0,1,5,4);
+	polygon(3,2,6,7);
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	gluLookAt(viewer[0],viewer[1],viewer[2],0,0,0,0,1,0);
+
+	glRotatef(theta[0],1,0,0);
+	glRotatef(theta[1],0,1,0);
+	glRotatef(theta[2],0,0,1);
+
+	colorcube();
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+
+void mouse(int btn,int state,int x,int y)
+{
+	if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
+		axis=0;
+	if(btn==GLUT_MIDDLE_BUTTON && state==GLUT_DOWN)
+		axis=1;
+	if(btn==GLUT_RIGHT_BUTTON && state==GLUT_DOWN)
+		axis=2;
+	
+	theta[axis]+=2.0;
+	if(theta[axis]>360.0) theta[axis]-=360.0;
+
+	display();
+}
+
+void keys(unsigned char key,int x,int y)
+{
+	if(key=='x') viewer[0]-=1;
+	if(key=='X') viewer[0]+=1;
+	if(key=='y') viewer[1]-=1;
+	if(key=='Y') viewer[1]+=1;
+	if(key=='z') viewer[2]-=1;
+	if(key=='Z') viewer[2]+=1;
+
+	display();
+
+}
+
+void reshape(int w,int h)
+{
+	glViewport(0,0,w,h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glOrtho(-2,2,-2,2,-2,2);
+	if(w<=h)
+		glFrustum(-2,2,-2*(GLfloat)h/(GLfloat)w,2*(GLfloat)h/(GLfloat)w,2,20);
+	else
+		glFrustum(-2*(GLfloat)w/(GLfloat)h,2*(GLfloat)w/(GLfloat)h,-2,2,2,20);
+
+	glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc,char **argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+	
+	glutCreateWindow("cube");
+
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keys);
+	glutMouseFunc(mouse);
+
+	glEnable(GL_DEPTH_TEST);
+	glutMainLoop();
+}
